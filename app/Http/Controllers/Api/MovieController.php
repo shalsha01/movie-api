d5a5bca11515072b05e0d850b63855472f87b9e6
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-03 15:57:55 +0300

    add MovieController with method to fetch popular movies from TMDB API

---
+++ app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\Controller;
+
+class MovieController extends Controller
+{
+    public function getPopularMovies()
+    {
+        $apiKey = env('TMDB_API_KEY');
+        $response = Http::get("https://api.themoviedb.org/3/movie/popular?api_key={$apiKey}");
+
+        if ($response->successful()) {
+            return response()->json($response->json(), 200);
+        }
+
+        return response()->json(['error' => 'Failed to fetch movies'], 500);
+    }
+}
