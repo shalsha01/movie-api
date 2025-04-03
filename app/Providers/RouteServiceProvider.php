2458d56e5eaea44affcaec2576fc9e33efcb2960
Shahad Haider <139572455+shalsha01@users.noreply.github.com>
2025-04-03 17:47:52 +0300

    refactor RouteServiceProvider and api routes for improved structure and clarity

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,12 +34,12 @@ class RouteServiceProvider extends ServiceProvider
      * @return void
      */
     protected function mapApiRoutes()
-    {
-        Route::prefix('api') // API routes should have 'api' as a prefix
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
-    }
+{
+    Route::prefix('api') // تأكد من أن هذا هو الـ prefix
+         ->middleware('api')
+         ->namespace($this->namespace)
+         ->group(base_path('routes/api.php'));
+}
 
     /**
      * Define the "web" routes for the application.
--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MovieController;
 
-// API Routes
 Route::prefix('movies')->group(function () {
-    // get popular movies
     Route::get('/popular', [MovieController::class, 'getPopularMovies']);
-    // get movie details by id
-    Route::get('/{id}', [MovieController::class, 'getMovieDetails']);
-
-    // search movies 
-    Route::get('/search', [MovieController::class, 'searchMovies']);
-
-    // (Upcoming moveies) 
-    Route::get('/upcoming', [MovieController::class, 'getUpcomingMovies']);
 });
-
